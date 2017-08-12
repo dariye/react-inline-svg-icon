c8b57dc30e59c1df2702ef05389c7793d1ddce4a
Paul Dariye <paul.dariye@gmail.com>
2017-08-12 11:44:14 -0400

    Add more icons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
 import { configure } from '@storybook/react';
+import { setDefaults } from '@storybook/addon-info';
+
+// addon-info
+setDefaults({
+  inline: true,
+  maxPropsIntoLine: 1,
+  maxPropObjectKeys: 10,
+  maxPropArrayLength: 10,
+  maxPropStringLength: 100,
+});
 
 function loadStories() {
   require('../stories');
--- stories/index.js
+++ stories/index.js
@@ -3,10 +3,25 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
-import { Button, Welcome } from '@storybook/react/demo';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import { Icon } from 'react-inline-svg-icon';
 
-storiesOf('Icon', module)
-  .add('default', () => <div><Icon /></div>)
+const stories = storiesOf('Icons', module);
+
+stories.addDecorator(withKnobs);
+
+stories
+  .add('default',
+    withInfo('without any props')
+    (() => (
+      <Icon color={text('Color', '#E74C3C')} size={number('Size', 25)} title={text('Title', 'I heart you')} />
+    ))
+  )
+  .add('add',
+    withInfo('without any props')
+    (() => (
+      <Icon />
+    ))
+  )
